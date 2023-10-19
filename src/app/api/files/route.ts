4df22d1ed815aa84c029b56ce6f84b463b6ab910
Tomer <tomerrim@gmail.com>
2023-10-19 21:09:12 +0300

    added pages, components, lib and worked on the API

---
+++ src/app/api/files/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import pdf from "@cyber2024/pdf-parse-fixed";
+import { createOrReadVectorStoreIndex } from "@/lib/vector-store";
+
+export async function POST(req: Request) {
+    try {
+        const formData = await req.formData();
+        const file = formData.get("file") as File;
+        const fileContents = await file.arrayBuffer();
+        const parsedPdf = await pdf(Buffer.from(fileContents));
+
+        await createOrReadVectorStoreIndex(parsedPdf.text);
+
+        return NextResponse.json({
+            message: "File Uploaded",
+        }, {
+            status: 200,
+        })
+    } catch (err) {
+        return NextResponse.json({
+            message: err.message,
+        }, {
+            status: 500,
+        })
+    }
+}
